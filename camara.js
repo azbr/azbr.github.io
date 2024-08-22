ade38b5abf1079ffdb0779ebc3bd78f1f519e483
Glauco <gazbr234@gmail.com>
2024-08-22 01:12:04 -0300

    Optimizing camara script and fixing some UI issues

--- camara.js
+++ camara.js
@@ -1,40 +1,40 @@
 // Esse arquivo será responsável por controlar a chart com os dados
 // das câmaras municipais.
 
-function ready3(error, vereadores) {
+function drawSankey(error, vereadores) {
   // dados dos vereadores eleitos do RJ de 2004 a 2016
-  window.vereadores = vereadores;
+  // window.vereadores = vereadores;
   // AINDA NÃO FUNCIONA!
   // TODO: Consertar essa coisa.
-  window.geraGrafo = function (d, anos, id) {
+  // window.geraGrafo = function (d, anos, id) {
 
-    var graph = {
-      "nodes": [],
-      "links": []
-    };
-    if (anos.length > 1) {
-      for (var k = 0; k < anos.length - 1; k++) {
-        for (var i in d[anos[k]][id].SIGLAS) {
-          graph.nodes.push({
-            "name": d[anos[k]] + "_" + d[anos[k]][id].SIGLAS[i].ORIENTACAO
-          });
-          graph.nodes.push({
-            "name": d[anos[k + 1]] + "_" + d[anos[k + 1]][id].SIGLAS[i].ORIENTACAO
-          }); +
-          graph.links.push({
-            "source": ENUM_ORIENTACAO[d[anos[k]][id].SIGLAS[i].ORIENTACAO],
-            "target": ENUM_ORIENTACAO[d[anos[k + 1]][id].SIGLAS[i].ORIENTACAO],
-            "value": d[anos[k]][id].SIGLAS[i].CONT
-          });
-          // console.log(d[ano][id].SIGLAS);
-        }
-      }
-    } else {
-      console.log("ERRO: O parametro anos deve ter no minimo 2 elementos.");
-      return undefined;
-    }
-    return graph;
-  };
+  //   var graph = {
+  //     "nodes": [],
+  //     "links": []
+  //   };
+  //   if (anos.length > 1) {
+  //     for (var k = 0; k < anos.length - 1; k++) {
+  //       for (var i in d[anos[k]][id].SIGLAS) {
+  //         graph.nodes.push({
+  //           "name": d[anos[k]] + "_" + d[anos[k]][id].SIGLAS[i].ORIENTACAO
+  //         });
+  //         graph.nodes.push({
+  //           "name": d[anos[k + 1]] + "_" + d[anos[k + 1]][id].SIGLAS[i].ORIENTACAO
+  //         }); +
+  //         graph.links.push({
+  //           "source": ENUM_ORIENTACAO[d[anos[k]][id].SIGLAS[i].ORIENTACAO],
+  //           "target": ENUM_ORIENTACAO[d[anos[k + 1]][id].SIGLAS[i].ORIENTACAO],
+  //           "value": d[anos[k]][id].SIGLAS[i].CONT
+  //         });
+  //         // console.log(d[ano][id].SIGLAS);
+  //       }
+  //     }
+  //   } else {
+  //     console.log("ERRO: O parametro anos deve ter no minimo 2 elementos.");
+  //     return undefined;
+  //   }
+  //   return graph;
+  // };
 
   const UPLOAD_FILENAME = "camara-rj.csv";
   const CHART_TITLE = "Câmara Municipal do Rio";
@@ -66,7 +66,8 @@ function ready3(error, vereadores) {
 
   function translate(x, y) { return `translate(${x}, ${y})` };
 
-  const color = d3.scale.linear()
+  const color = d3.scale
+    .linear()
     .domain([1, 6])
     .interpolate(d3.interpolateRgb)
     .range(["red", "blue"]);
@@ -110,19 +111,20 @@ function ready3(error, vereadores) {
       graph.links.push({
         "source": d.source,
         "target": d.target,
-        "value": +d.value
+        "value": parseInt(d.value)
+      });
+      console.log({
+        "source": d.source,
+        "target": d.target,
+        "value": parseInt(d.value)
       });
     });
 
     // return only the distinct / unique nodes
-    graph.nodes = d3.keys(d3.nest()
-      .key(function (d) {
-        return d.name;
-      })
-      .map(graph.nodes));
+    graph.nodes = d3.keys(d3.nest().key(d => d.name).map(graph.nodes));
 
     // loop through each link replacing the text with its index from node
-    graph.links.forEach((d, i) => {
+    graph.links.forEach(d => {
       d.source = graph.nodes.indexOf(d.source);
       d.target = graph.nodes.indexOf(d.target);
     });
@@ -144,35 +146,29 @@ function ready3(error, vereadores) {
       .selectAll(".link")
       .data(graph.links)
       .enter().append("path")
-      .attr("class", "link")
+      .attr("class", d => `link ${!d.value ? 'zero' : ''}`)
       .attr("d", path)
       .style("stroke-width", function (d) {
         return Math.max(1, d.dy);
       })
-      .sort(function (a, b) {
-        return b.dy - a.dy;
-      });
+      .sort((a, b) => b.dy - a.dy);
 
     // add the link titles
     link.append("title")
-      .text(function (d) {
-        return d.source.name + "  " + d.target.name + "\n" + format(d.value);
-      });
-
+      .text(d => `${d.source.name} ${d.target.name} \n ${d.value}`);
+    
+    console.log(graph.nodes);
     // add in the nodes
-    var node = svg.append("g")
+    const node = svg.append("g")
       .attr("transform", translate(...sankey_nodes))
       .selectAll(".node")
       .data(graph.nodes)
-      .enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function (d) {
-        return "translate(" + d.x + "," + d.y + ")";
-      })
+      .enter()
+      .append("g")
+      .attr("class", d => `node ${!d.value ? 'zero' : ''}`)
+      .attr("transform", d => translate(d.x, d.y))
       .call(d3.behavior.drag()
-        .origin(function (d) {
-          return d;
-        })
+        .origin(d => d)
         .on("dragstart", function () {
           this.parentNode.appendChild(this);
         })
@@ -184,44 +180,30 @@ function ready3(error, vereadores) {
         return d.dy;
       })
       .attr("width", sankey.nodeWidth())
-      .style("fill", function (d) {
-        //TODO: Melhorar isso aqui depois!
-        //   return d.color = color(d.name.replace(/ .*/, "")); })
-        return d.color = color(d.name[5]);
-      })
-      .style("stroke", function (d) {
-        return d3.rgb(d.color).darker(2);
-      })
+      .style("fill", d => d.color = color(d.name[6]) )
+      .style("stroke", d => d3.rgb(d.color).darker(2) )
       .append("title")
-      .text(function (d) {
-        return d.name + "\n" + format(d.value);
-      });
+      .text(d => d.name + "\n" + format(d.value) );
 
     // add in the title for the nodes
     node.append("text")
       .attr("x", -6)
-      .attr("y", function (d) {
-        return d.dy / 2;
-      })
+      .attr("y", d => d.dy / 2)
       .attr("dy", ".35em")
       .attr("text-anchor", "end")
       .attr("transform", null)
-      .text(function (d) {
-        return d.name;
-      })
-      .filter(function (d) {
-        return d.x < chartWidth / 2;
-      })
+      .text(d => d.name)
+      .filter(d => d.x < chartWidth / 2)
       .attr("x", 6 + sankey.nodeWidth())
       .attr("text-anchor", "start");
 
     // the function for moving the nodes
     function dragmove(d) {
+      
+      d.y = Math.max(0, Math.min(chartHeight - d.dy, d3.event.y));
       d3.select(this)
-      .attr("transform",
-        "translate(" + d.x + "," + (
-          d.y = Math.max(0, Math.min(chartHeight - d.dy, d3.event.y))
-        ) + ")");
+      .attr("transform", 
+        translate(d.x, d.y));
       sankey.relayout();
       link.attr("d", path);
     }
@@ -231,7 +213,8 @@ function ready3(error, vereadores) {
 
 $().ready(() => {
   // Leitura dos dados dos vereadores
-  d3.queue()
-    .defer(d3.json, "vereadores1.json")
-    .await(ready3);
+  // d3.queue()
+  //   .defer(d3.json, "vereadores1.json")
+  //   .await(drawSankey);
+    drawSankey({}, {});
 });
